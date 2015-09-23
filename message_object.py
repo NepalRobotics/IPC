f4c4a4f4c7a84c8f127ab6e26c68bf0c861166e8
Daniel Petti <djpetti@gmail.com>
2015-09-22 21:46:02 -0400

    Make indentation consistent.
    We flipped a coin, and settled on two spaces.

--- message_object.py
+++ message_object.py
@@ -2,100 +2,92 @@
 This module contains classes for sending/receiving encoded data over
 IPC sockets.
 """
+
+
 import json, inspect
+
+
 class MessageObject(object):
+  """
+  Class representing an IPC message
+  """
+  def __init__(self):
     """
-    Class representing an IPC message
+    Construct a new MessageObject object.
+
+    :return:
     """
-    def __init__(self):
-        """
-        Construct a new MessageObject object.
+  def primitives(self):
+    """Return a serializable dictionary of self's contents
 
-        :return:
-        """
-    def primitives(self):
-        """Return a serializable dictionary of self's contents
+    Args:
 
-        Args:
+    Returns:
+        A dictionary of self's primitives
+    """
+    prim_dict = {}
+    for key in self.__dict__:
+      elem = self.__dict__[key]
+      # We determine whether elem is a serializable object, or whether
+      # we need to recursively call the primitives operator on a
+      # MessageObject
+      # NOTE: this assumes all but message objects are serializable, so
+      # make sure to only use serializables or to override encode() when
+      # making subclasses of MessageObject
 
-        Returns:
-            A dictionary of self's primitives
-        """
-        prim_dict = {}
-        for key in self.__dict__:
-            elem = self.__dict__[key]
-            # We determine whether elem is a serializable object, or whether
-            # we need to recursively call the primitives operator on a
-            # MessageObject
-            # NOTE: this assumes all but message objects are serializable, so
-            # make sure to only use serializables or to override encode() when
-            # making subclasses of MessageObject
+      if MessageObject in inspect.getmro(type(elem)):
+        prim_dict[key] = elem.primitives()
+      else:
+        prim_dict[key] = elem
+    return prim_dict
 
-            if MessageObject in inspect.getmro(type(elem)):
-                prim_dict[key] = elem.primitives()
-            else:
-                prim_dict[key] = elem
-        return prim_dict
-
-    def encode(self):
-        """
-        Create an encoded version of self
-        :return: Returns encoded self
-        """
-        return json.dumps(self.primitives())
+  def encode(self):
+    """
+    Create an encoded version of self
+    :return: Returns encoded self
+    """
+    return json.dumps(self.primitives())
 
 class VehicleState(MessageObject):
-    """
-    Encodable UAV craft state information
-    """
-    def __init__(self, vehicle_uid, **kwargs):
-        """Construct a new VehicleState object
+  """
+  Encodable UAV craft state information
+  """
+  def __init__(self, vehicle_uid, **kwargs):
+    """Construct a new VehicleState object
 
-        Args:
-            vehicle_uid: a unique identifier string for the craft sending state
+    Args:
+        vehicle_uid: a unique identifier string for the craft sending state
 
-        Returns: returns nothing
-        """
-        self.vehicle_uid = vehicle_uid
-        self.vehicle_is_armed = kwargs.get("vehicle_is_armed")
-        self.attitude_pitch = kwargs.get("attitude_pitch")
-        self.attitude_yaw = kwargs.get("attitude_yaw")
-        self.attitude_roll = kawrgs.get("attitude_roll")
-        self.velocity_array = kwargs.get("velocity_array")
-        self.airspeed = kwargs.get("airspeed")
-        self.groundspeed = kwargs.get("groundspeed")
-        self.gps_fix_type = kwargs.get("gps_fix_type")
-        self.latitude = kwargs.get("latitude")
-        self.longitude = kwargs.get("longitude")
-        self.altitude_absolute = kwargs.get("altitude_absolute")
-        self.altitude_relative = kwargs.get("altitude_relative")
-        self.battery_level = kwargs.get("battery_level")
+    Returns: returns nothing
+    """
+    self.vehicle_uid = vehicle_uid
+    self.vehicle_is_armed = kwargs.get("vehicle_is_armed")
+    self.attitude_pitch = kwargs.get("attitude_pitch")
+    self.attitude_yaw = kwargs.get("attitude_yaw")
+    self.attitude_roll = kawrgs.get("attitude_roll")
+    self.velocity_array = kwargs.get("velocity_array")
+    self.airspeed = kwargs.get("airspeed")
+    self.groundspeed = kwargs.get("groundspeed")
+    self.gps_fix_type = kwargs.get("gps_fix_type")
+    self.latitude = kwargs.get("latitude")
+    self.longitude = kwargs.get("longitude")
+    self.altitude_absolute = kwargs.get("altitude_absolute")
+    self.altitude_relative = kwargs.get("altitude_relative")
+    self.battery_level = kwargs.get("battery_level")
 
 class RadioState(MessageObject):
-    """
-    Encodable Doppler Radio signal state information
-    """
-    def __init__(self):
-      # A list of all the signals received in a given time-frame, generally
-      # assumed to be since we last received one of these messages.
-      self.__signals = []
-
-    def add_signal(self, lob, strength):
-      """ Add a new signal to the list of signals.
-      Args:
-        lob: The LOB to the object producing the signal.
-        strength: The strength of the signal. """
-      self.__signals.push_back((lob, strength))
-
-    def signals(self):
-      """
-      Returns:
-        All the signals stored in this structure, as a list. """
-      return self.__signals
+  """
+  Encodable Doppler Radio signal state information
+  """
+  def __init__(self, **kwargs):
+    # The LOB to the signal source.
+    self.lob = kwargs.get("lob")
+    # The strength of the signal.
+    self.strength = kwargs.get("strength")
 
 class LogEvent(MessageObject):
-    """
-    Encodable information for recording to system log
-    """
-    #TODO: implement
-    pass
+  """
+  Encodable information for recording to system log
+  """
+  #TODO: implement
+  pass
