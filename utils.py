b4779e0d6366cef3040476b5453799010c2e1c34
Daniel Petti <djpetti@gmail.com>
2015-12-26 11:54:05 -0500

    Add new class for test queues.
    These are queues that expose the same API as true queues, but are
    made explicitly for single-threaded testing.

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 """ Standard shared utilities. """
 
 
+from collections import deque
+import Queue
+
 import time
 
 
@@ -37,3 +40,43 @@ class PhasedLoopLimitter:
     sleep_for = max(0, self.__ticks - elapsed)
     time.sleep(sleep_for)
     return new_time
+
+
+class TestQueue:
+  """ Real Python queues are not strongly consistent with puts, which makes them
+  somewhat impractical for testing purposes. Therefore, here we have a stupid
+  little class that has the same API as a real queue, but just buffers data for
+  testing purposes.
+  NOTE: Because it is meant to be used in a single-threaded environment,
+  blocking doesn't make sense at all. That means reading from an empty TestQueue
+  will just throw an exception. Because of this, blocking arguments are provided
+  for compatibility, but are all no-ops. """
+
+  def __init__(self):
+    # This is what actually stores data.
+    self.__buffer = deque()
+
+  def put(self, item, block=True):
+    """ Puts an item on the fake queue.
+    Args:
+      item: The item to put.
+      block: No-op. """
+    self.__buffer.appendleft(item)
+
+  def put_nowait(self, item):
+    self.put(item, block=False)
+
+  def get(self, block=True):
+    """ Removes an item from the fake queue. Reading from an empty queue raises
+    a Queue.Empty exception.
+    Args:
+      block: no-op.
+    Returns:
+      The item that was removed. """
+    if not len(self.__buffer):
+      raise Queue.Empty()
+
+    return self.__buffer.pop()
+
+  def get_nowait(self):
+    return self.get(block=False)
